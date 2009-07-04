824ad60d6d15075fd4f51f8fabb2d33ca1cf16a5
Tavian Barnes <tavianator@gmail.com>
2009-07-04 22:00:25 +0000

    Clarify test suite license.

--- tests/error.c
+++ tests/error.c
@@ -3,19 +3,18 @@
  *                                                                       *
  * This file is part of The Dimension Test Suite.                        *
  *                                                                       *
- * Dimension is free software; you can redistribute it and/or modify it  *
- * under the terms of the GNU Lesser General Public License as published *
- * by the Free Software Foundation; either version 3 of the License, or  *
- * (at your option) any later version.                                   *
+ * The Dimension Test Suite is free software; you can redistribute it    *
+ * and/or modify it under the terms of the GNU General Public License as *
+ * published by the Free Software Foundation; either version 3 of the    *
+ * License, or (at your option) any later version.                       *
  *                                                                       *
- * Dimension is distributed in the hope that it will be useful, but      *
- * WITHOUT ANY WARRANTY; without even the implied warranty of            *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
- * Lesser General Public License for more details.                       *
+ * The Dimension Test Suite is distributed in the hope that it will be   *
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty   *
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  *
+ * General Public License for more details.                              *
  *                                                                       *
- * You should have received a copy of the GNU Lesser General Public      *
- * License along with this program.  If not, see                         *
- * <http://www.gnu.org/licenses/>.                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
  *************************************************************************/
 
 /* Make sure that errors terminate us - this test should fail */
