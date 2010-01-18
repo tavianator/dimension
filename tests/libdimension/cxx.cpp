afa0a7ee31d4956f99f55e2c423ba20a06409bc7
Tavian Barnes <tavianator@gmail.com>
2010-01-17 23:38:36 -0500

    Add a C++ test.

---
+++ tests/libdimension/cxx.cpp
@@ -0,0 +1,32 @@
+/*************************************************************************
+ * Copyright (C) 2008 Tavian Barnes <tavianator@gmail.com>               *
+ *                                                                       *
+ * This file is part of The FPFD Library Test Suite.                     *
+ *                                                                       *
+ * The FPFD Library Test Suite is free software; you can redistribute it *
+ * and/or modify it under the terms of the GNU General Public License as *
+ * published by the Free Software Foundation; either version 3 of the    *
+ * License, or (at your option) any later version.                       *
+ *                                                                       *
+ * The FPFD Library Test Suite is distributed in the hope that it will   *
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied         *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See *
+ * the GNU General Public License for more details.                      *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *************************************************************************/
+
+#include "tests.h"
+
+/*
+ * Ensure that dimension.h can be included in a C++ program and behave properly.
+ */
+int
+main()
+{
+  /* Make sure we can to bit-ops on this enum type */
+  dmnsn_quality quality = DMNSN_RENDER_PIGMENT | DMNSN_RENDER_LIGHTS;
+
+  return EXIT_SUCCESS;
+}
