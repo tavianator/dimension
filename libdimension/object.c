9e117cb576f55e3215cb46dcd8d2ad3b52124eb4
Tavian Barnes <tavianator@gmail.com>
2010-02-25 15:27:53 -0500

    Forgot to dmnsn_delete_interior() in dmnsn_delete_object().

--- libdimension/object.c
+++ libdimension/object.c
@@ -58,6 +58,7 @@ void
 dmnsn_delete_object(dmnsn_object *object)
 {
   if (object) {
+    dmnsn_delete_interior(object->interior);
     dmnsn_delete_texture(object->texture);
     if (object->free_fn) {
       (*object->free_fn)(object->ptr);
