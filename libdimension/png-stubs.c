e0313016725bc9f87243571e75b2e77f1cae265e
Tavian Barnes <tavianator@gmail.com>
2010-04-01 14:45:42 -0400

    Add --disable-png configure option.

---
+++ libdimension/png-stubs.c
@@ -0,0 +1,53 @@
+/*************************************************************************
+ * Copyright (C) 2010 Tavian Barnes <tavianator@gmail.com>               *
+ *                                                                       *
+ * This file is part of The Dimension Library.                           *
+ *                                                                       *
+ * The Dimension Library is free software; you can redistribute it and/  *
+ * or modify it under the terms of the GNU Lesser General Public License *
+ * as published by the Free Software Foundation; either version 3 of the *
+ * License, or (at your option) any later version.                       *
+ *                                                                       *
+ * The Dimension Library is distributed in the hope that it will be      *
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty   *
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  *
+ * Lesser General Public License for more details.                       *
+ *                                                                       *
+ * You should have received a copy of the GNU Lesser General Public      *
+ * License along with this program.  If not, see                         *
+ * <http://www.gnu.org/licenses/>.                                       *
+ *************************************************************************/
+
+#include "dimension.h"
+
+/* Stubs for PNG functions when compiled with --disable-png */
+
+int
+dmnsn_png_optimize_canvas(dmnsn_canvas *canvas)
+{
+  return 1;
+}
+
+int
+dmnsn_png_write_canvas(const dmnsn_canvas *canvas, FILE *file)
+{
+  return 1;
+}
+
+dmnsn_progress *
+dmnsn_png_write_canvas_async(const dmnsn_canvas *canvas, FILE *file)
+{
+  return NULL;
+}
+
+dmnsn_canvas *
+dmnsn_png_read_canvas(FILE *file)
+{
+  return NULL;
+}
+
+dmnsn_progress *
+dmnsn_png_read_canvas_async(dmnsn_canvas **canvas, FILE *file)
+{
+  return NULL;
+}
