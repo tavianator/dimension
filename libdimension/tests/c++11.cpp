2ac69f37ad5da0aeaffe89e40de4e867c19fa65f
Tavian Barnes <tavianator@tavianator.com>
2014-08-11 17:14:54 -0400

    Add a C++11 test, and make the C++98/03 test explicit.

---
+++ libdimension/tests/c++11.cpp
@@ -0,0 +1,38 @@
+/*************************************************************************
+ * Copyright (C) 2010-2011 Tavian Barnes <tavianator@tavianator.com>     *
+ *                                                                       *
+ * This file is part of The Dimension Test Suite.                        *
+ *                                                                       *
+ * The Dimension Test Suite is free software; you can redistribute it    *
+ * and/or modify it under the terms of the GNU General Public License as *
+ * published by the Free Software Foundation; either version 3 of the    *
+ * License, or (at your option) any later version.                       *
+ *                                                                       *
+ * The Dimension Test Suite is distributed in the hope that it will be   *
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty   *
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  *
+ * General Public License for more details.                              *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *************************************************************************/
+
+/**
+ * @file
+ * Ensure that dimension.h can be included in a C++ program and behave properly.
+ */
+
+#include "tests.h"
+#include <cstdlib>
+
+int
+main(void)
+{
+  // Treat warnings as errors for tests
+  dmnsn_die_on_warnings(true);
+
+  // Make sure we can to bit-ops on this enum type
+  dmnsn_quality quality = DMNSN_RENDER_PIGMENT | DMNSN_RENDER_LIGHTS;
+
+  return EXIT_SUCCESS;
+}
